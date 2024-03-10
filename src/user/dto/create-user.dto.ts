b0f866a7be56b0df32844e2ad832c72382b1c740
Fiido <roccobosio@gmail.com>
2024-03-10 11:23:04 -0300

    validate dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,19 @@
+import { IsNotEmpty, IsString } from "class-validator"
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
   firstname: string
+
+  @IsNotEmpty()
+  @IsString()
   lastname: string
+
+  @IsNotEmpty()
+  @IsString()
   email: string
+
+  @IsNotEmpty()
+  @IsString()
   password: string
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common"
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from "@nestjs/common"
 import { UserService } from "./user.service"
 import { CreateUserDto } from "./dto/create-user.dto"
+import { UpdateUserDto } from "./dto/update-user.dto"
 
 @Controller("user")
 export class UserController {
@@ -19,5 +28,15 @@ export class UserController {
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto)
+  }
+
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto)
+  }
+
+  @Delete(":id")
+  delete(@Param("id") id: string) {
+    return this.userService.delete(+id)
   }
 }
