0a248a26b5fed3e22741517d01f97c83b2e3ef57
Fiido <roccobosio@gmail.com>
2024-03-11 19:57:46 -0300

    change conn db to uri

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -8,14 +8,7 @@ import { join } from "path"
     TypeOrmModule.forRootAsync({
       useFactory: (configService: ConfigService) => ({
         type: "mysql",
-        host: "mysql",
-        port: 3306,
-        username: configService.getOrThrow("MYSQL_USER"),
-        password: configService.getOrThrow("MYSQL_PASSWORD"),
-        database: configService.getOrThrow("MYSQL_DATABASE"),
-        entities: [join(__dirname, "../entities/*.entity{.ts,.js}")],
-        synchronize: configService.getOrThrow("MYSQL_SYNCHRONIZE"),
-        dropSchema: configService.getOrThrow("MYSQL_DROP"),
+        url: configService.getOrThrow("MYSQL_URI"),
       }),
       inject: [ConfigService],
     }),
